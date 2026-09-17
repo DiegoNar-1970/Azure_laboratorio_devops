0d4acca86d5deebb576872f21f11bac2e1243290
Diego <diegonaranjo303@gmail.com>
2026-09-16 19:25:45 -0500

    cambio pequeño

---
+++ Front/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>¡Bienvenido !</h1>
+      <p>Nos alegra tenerte aquí 🚀</p>
+    </div>
+    </>
+  )
+}
+
+export default App
